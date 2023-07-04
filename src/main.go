d958dba90b31b7198dea6f8e1177807fd7a481d7
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-04 21:08:39 +0900

    テンプレート使った

--- src/main.go
+++ src/main.go
@@ -1,12 +1,20 @@
 package main
 
-import	"github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	e := gin.Default()
+	e.LoadHTMLGlob("templates/*")
 	e.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "success",
+		// c.JSON(200, gin.H{
+		// 	"status": "success",
+		// })
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website!!",
 		})
 	})
 	e.GET("/hello", func(c *gin.Context) {
